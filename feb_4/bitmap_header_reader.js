d5cbf9ffe6ad4101c046e9c381ccf6707576aa22
callumsong <ghksong@gmail.com>
2015-02-04 18:14:03 -0800

    test is working - randomizing all colors

--- feb_4/bitmap_header_reader.js
+++ feb_4/bitmap_header_reader.js
@@ -13,5 +13,16 @@ bitmapObject.height = bitmap.readInt32LE(22);
 bitmapObject.colorDepth = bitmap.readInt16LE(28);
 bitmapObject.paletteSize = bitmap.readInt32LE(46);
 
+
+for (var i = 0; i < 1023; i++) {
+  bitmap[54 + i] = Math.floor(Math.random() * 256);
+}
+
+for (var i = 1078; i < 11078; i++) {
+  bitmap[i] = Math.floor(Math.random() * 256);
+}
 console.dir(bitmapObject);
 
+fs.writeFile('newTest.bmp', bitmap, 'utf-8', function (err, data){
+  console.log("Type 'open newTest.bmp' to view changed file");
+});
